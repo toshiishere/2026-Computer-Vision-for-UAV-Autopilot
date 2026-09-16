82f86cd8d7e3e5947762ccff1dcb878153a8c06c
TOSHI-S14WSL <toshi.cs12@nycu.edu.tw>
2026-09-16 14:18:02 +0800

    added q1

---
+++ lab1/1-1.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+img = cv2.imread('q1.jpg')
+
+B, G, R = cv2.split(img)
+
+# 找藍點
+blue_mask = (
+    (B > 100) & (B > 1.5 * R) & (B > 1.2 * G)
+)
+
+# 原圖轉灰階
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# 灰階轉回 3 channels
+gray_bgr = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+
+# 先讓整張圖變灰階
+result = gray_bgr.copy()
+
+# 藍點保留原本顏色
+result[blue_mask] = img[blue_mask]
+
+cv2.imshow("Original", img)
+cv2.imshow("Result", result)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.imwrite("q1.jpg", result)
